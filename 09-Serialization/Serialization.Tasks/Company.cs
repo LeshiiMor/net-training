3aafd6ae6cf01fe85340a56d9aa3dce8d7ae0cfa
Alexey <alepaca28@gmail.com>
2022-01-10 18:51:43 +0300

    completed Company.cs

--- 09-Serialization/Serialization.Tasks/Company.cs
+++ 09-Serialization/Serialization.Tasks/Company.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Serialization.Tasks
 {
@@ -7,24 +8,42 @@ namespace Serialization.Tasks
     // Company class has to be forward compatible with all derived versions
 
  
-    public class Company
+    [DataContract]
+    public class Company: IExtensibleDataObject
     {
+        [DataMember]
         public string Name { get; set; }
+
+        [DataMember(Name= "Employee")]
         public IList<Employee> Employee { get; set; }
+
+        ExtensionDataObject IExtensibleDataObject.ExtensionData { get; set; }
     }
 
+    [DataContract,KnownType(typeof(Worker)),KnownType(typeof(Manager))]
     public abstract class Employee {
+
+        [DataMember]
         public string Name { get; set; }
+
+        [DataMember]
         public string LastName { get; set; }
+
+        [DataMember]
         public string Title { get; set; }
         public Manager Manager { get; set; }
     }
 
+    [DataContract]
     public class Worker : Employee {
+        [DataMember]
         public int Salary { get; set; }
     }
 
+    [DataContract]
     public class Manager : Employee {
+
+        [DataMember]
         public int YearBonusRate { get; set; } 
     }
 
